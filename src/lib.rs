6f00dfadd6f8d83840e148bf7169c89692dd50a0
Kayh <basedlainpilled@gmail.com>
2024-01-11 22:58:51 -0500

    add links

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,10 @@ fn App() -> impl IntoView {
     view! {
         <div class="max-w-2xl mx-auto py-8 px-4">
             <h1 class="text-center pb-2">"UNAVI"</h1>
+            <nav class="space-x-2 justify-center flex">
+                <a href="https://docs.unavi.xyz">"[docs]"</a>
+                <a href="https://app.unavi.xyz">"[app]"</a>
+            </nav>
         </div>
     }
 }
